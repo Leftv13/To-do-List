f8a810dc3fbe342b7a3161648529b23b7abe39ee
Leftv13 <andresabreu007@gmail.com>
2025-02-21 09:03:15 -0400

    Responsiveness final version

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+const taskframe = () =>{
+    document.querySelector(".task-frame");
+} ;
+
+const addTask = () =>{
+const addtsk = document.createElement(".task")
+addTask.innertext = (".input")
+
+};
+
+const addbutton = document.querySelector(".addbttn")
+addbutton.addEventListener("click" , "addtask")
+
+taskframe.append(addTask);
+
+
+
